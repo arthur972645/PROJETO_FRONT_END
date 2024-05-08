c69b41e5213fe05337add5b1f68a7b1bd548bc7b
arthur972645 <arthurall.1973@gmail.com>
2024-05-08 12:16:35 -0300

    frameworks

---
+++ FremeWork/src/App/App.jsx
@@ -0,0 +1,12 @@
+
+import Article from '../Article/Article'
+import './App.css'
+
+const App = () => {
+    return(
+        <>
+            <h1>Oi, Arthur!!</h1> 
+        </>
+    )
+}
+export default App
