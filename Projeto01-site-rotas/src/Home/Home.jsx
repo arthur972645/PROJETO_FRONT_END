61c24c6501f79f51c60e8eab3860c4886453979e
arthur972645 <arthurall.1973@gmail.com>
2024-05-22 11:50:03 -0300

    projeto01-site

---
+++ Projeto01-site-rotas/src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+const Home = () => {
+    return(
+        <section className='texto'>
+            <h2>SO, YOU WANT TO TRAVEL TO</h2>
+            <h1>SPACE</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic eveniet ipsum laudantium, fuga quasi dignissimos eligendi quae delectus assumenda sapiente laborum at voluptatum ullam fugit. Maiores voluptatibus mollitia aut illo.</p>
+        </section>
+    )
+} 
+export default Home
--- Rotas/src/projeto01/App/App.jsx
+++
@@ -1,8 +0,0 @@
-
-
-const App  = () => {
-    return(
-        <h1>rwr</h1>
-    )
-}
-export default App
